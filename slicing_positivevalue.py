67fe986558286926d35b252e15bd7e6acf0de1c4
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-06-24 12:30:55 +0530

    Add files via upload

---
+++ slicing_positivevalue.py
@@ -0,0 +1,27 @@
+# a='pravalika'
+# # print(a[:])      # pravalika   if we don't gave the value on right side it will show the total string
+# # print(a[1:])     # ravalika
+# # print(a[2:])     # avalika
+# # print(a[3:])     # valika
+# # print(a[4:])     # alika
+# # print(a[5:])     # lika
+# # print(a[6:])     # ika
+# # print(a[7:])     # ka
+# # print(a[8:])     # a
+
+# # print(a[:9])       # pravalika   if we gave the value on right side it will reduce one value & in left side it takes the value by defaul as 0
+# # print(a[:8])       # pravalik
+# # print(a[:7])       # pravali
+# # print(a[:6])       # praval
+# # print(a[:5])       # prava
+# # print(a[:4])       # prav
+# # print(a[:3])       # pra
+# # print(a[:2])       # pr
+# # print(a[:1])       # p
+
+# print(a[0:5])   # prava    left side value should be lessthan the right side value
+# print(a[1:5])   # rava
+
+# print(a[6:5])    # empty line  if the left side value is greaterthan the right side value it not shows the error ,bt it shows empty line 
+# print(a[3:2])    # empty line 
+# print(a[4:3])    # empty line
